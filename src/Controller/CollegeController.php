c8536c8d51bd054e4203e937f68700bf16a7c07c
marwen <mansourmarwen@outlook.fr>
2021-09-23 22:34:04 +0200

    lina

--- src/Controller/CollegeController.php
+++ src/Controller/CollegeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Prof;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CollegeController extends AbstractController
 {
@@ -26,5 +28,35 @@ class CollegeController extends AbstractController
         return $this->render('college/reglement.html.twig', [
             'controller_name' => 'CollegeController',
         ]);
+    }
+    /**
+     * @Route("/professeur/{id}/delete", name="delete_professeur")
+     */
+    public function delete(Prof $professeur, EntityManagerInterface $entityManager) {
+        /**
+         * On demande à Symfony de nous 
+         * récupérer automatiquement un produit
+         * 
+         * Il va tenter de la faire grâce à l'injection de dépendance
+         * 
+         * Pour trouver le bon produit, 
+         * il va prendre le premier paramètre d'URL
+         * et le considérer comme la clef primaire 
+         * qui permet de retrouver le produit en question
+         */
+
+        /**
+         * L'entity Manager nous sert 
+         * à MODIFIER, CREER, SUPPRIMER des entités
+         * 
+         * On l'injecte grâce à l'injection de dépendance
+         */
+
+        // dd($produit); // dump & die
+
+        $entityManager->remove($professeur); // On ajoute le delete à la TODO-list
+        $entityManager->flush(); // On exécute la TODO-list
+
+        return $this->redirectToRoute('acceuil');
     }
 }
